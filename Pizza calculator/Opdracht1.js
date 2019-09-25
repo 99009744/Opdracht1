9dd064044970cf96acb75da8665b27e772813e14
99009744 <99009744@mydavinci.nl>
2019-09-25 14:46:05 +0200

    pizza
    pizza bijna klaar alleen breaks toevoegen

--- Pizza calculator/Opdracht1.js
+++ Pizza calculator/Opdracht1.js
@@ -1,7 +1,11 @@
-var vraag1 = prompt("Wilt u een pizza bestellen?")
-var vraag2 = prompt("small medium of large?")
-var vraag3 = prompt("hoeveel?")
-const SMALL = 4;
-const MEDIUM = 7;
-const LARGE = 10;
-document.write (vraag2+" "+"pizza x "+ " "+vraag3)
+var smallAmount = parseInt(prompt("Hoeveel small pizza's wilt u?"))
+var mediumAmount = parseInt(prompt("Hoeveel medium pizza's wilt u?"))
+var largeAmount = parseInt(prompt("Hoeveel large pizza's wilt u?"))
+
+const SMALLPRICE = 4;
+const MEDIUMPRICE = 7;
+const LARGEPRICE = 10;
+
+document.write ("small pizza's" + " "+smallAmount+" " + "met een prijs van"+ smallAmount*SMALLPRICE+br)
+document.write ("medium pizza's" + " "+mediumAmount+" " + "met een prijs van"+ mediumAmount*MEDIUMPRICE+br)
+document.write ("large pizza's" +" "+largeAmount+" " + "met een prijs van"+ largeAmount*LARGEPRICE+br)
