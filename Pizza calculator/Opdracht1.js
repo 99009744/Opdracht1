740a8dca9441699e077c79fa735bbcffe740d159
99009744 <99009744@mydavinci.nl>
2019-09-27 09:12:36 +0200

    Total amount toegevoegd
    total amount toegevoegd

--- Pizza calculator/Opdracht1.js
+++ Pizza calculator/Opdracht1.js
@@ -5,7 +5,13 @@ var largeAmount = parseInt(prompt("Hoeveel large pizza's wilt u?"))
 const SMALLPRICE = 4;
 const MEDIUMPRICE = 7;
 const LARGEPRICE = 10;
+const TOTALSMALL =SMALLPRICE*smallAmount;
+const TOTALMEDIUM =MEDIUMPRICE*mediumAmount;
+const TOTALLARGE =LARGEPRICE*largeAmount;
 
-document.write ("small pizza's" + " "+smallAmount+" " + "met een prijs van"+ smallAmount*SMALLPRICE+br)
-document.write ("medium pizza's" + " "+mediumAmount+" " + "met een prijs van"+ mediumAmount*MEDIUMPRICE+br)
-document.write ("large pizza's" +" "+largeAmount+" " + "met een prijs van"+ largeAmount*LARGEPRICE+br)
+
+document.write ("small pizza's" + " "+smallAmount+" " + "met een prijs van"+ " " +smallAmount*SMALLPRICE+"<br>")
+document.write ("medium pizza's" + " "+mediumAmount+" " + "met een prijs van"+ " " +mediumAmount*MEDIUMPRICE+"<br>")
+document.write ("large pizza's" +" "+largeAmount+" " + "met een prijs van"+ " " +largeAmount*LARGEPRICE+"<br>")
+var totalamount = parseInt(TOTALSMALL+TOTALMEDIUM+TOTALLARGE)
+document.write ("Totaal bedrag van" + " "+totalamount)
